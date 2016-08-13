f5d99c09e28ec7c5ad3c9a7d33a7ff83317441bd
Logan Elliott <logansails@gmail.com>
2016-08-13 18:22:30 +1200

    Add version api

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/version', function(request, response) {
+	  response.send('Version: 1.0.0')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
